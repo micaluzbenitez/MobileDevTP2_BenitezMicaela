684f7aa5f243936e9b3219c0ca23858c93396e2f
micalbenitez <benitezmicaelaluz@gmail.com>
2022-10-14 11:39:15 -0300

    Updated plugin

--- MobileDevTP2_BenitezMicaela/Assets/Scripts/TestPlugin.cs
+++ MobileDevTP2_BenitezMicaela/Assets/Scripts/TestPlugin.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -8,13 +9,45 @@ public class TestPlugin : MonoBehaviour
 
     private void Start()
     {
-        //logger = LoggerBase.
+        logger = LoggerBase.CreateLogger();
+        UpdateLogs();
+    }
+
+    #region UI_Events
+    public void SendLog()
+    {
+        logger.Log("Time: " + Time.time);
+        UpdateLogs();
+    }
+
+    public void ClearLog()
+    {
+        // Llamar a la alerta con el callback de ConfirmDeleteAll()
+        logger.ShowAlert(ConfirmDeleteAll);
+    }
+    #endregion
+
+    private void ConfirmDeleteAll(bool confirm)
+    {
+        if (!confirm) return;
+
+        logger.DeleteAll();
+        UpdateLogs();
+    }
+    
+    private void UpdateLogs()
+    {
+        var logs = logger.GetAllLogs();
+        // Updatear logs en la pantalla
     }
 }
 
 public abstract class LoggerBase
 {
     public abstract void Log(string str);
+    public abstract void DeleteAll();
+    public abstract List<string> GetAllLogs();
+    public abstract void ShowAlert(System.Action<bool> callback);
 
     public static LoggerBase CreateLogger()
     {
@@ -29,7 +62,7 @@ public abstract class LoggerBase
 public class AndroidLogger : LoggerBase
 {
     const string LOGGER_CLASS_NAME = "com.example.logger2022.GameLogger";
-
+    const char SEP = ';';
     AndroidJavaClass loggerClass;
     AndroidJavaObject loggerObject;
 
@@ -41,7 +74,28 @@ public class AndroidLogger : LoggerBase
 
     public override void Log(string str)
     {
-        loggerObject.Call("MyLog", "BOTON");
+        loggerObject.Call("MyLog", str);
+    }
+
+    public override void DeleteAll()
+    {
+        loggerObject.Call("DeleteAll");
+    }
+
+    public override List<string> GetAllLogs()
+    {
+        List<string> allLogs = new List<string>();
+        string text = loggerObject.Call<string>("GetAllLogs");
+        var logsArray = text.Split(SEP);
+        allLogs.AddRange(logsArray);
+        return allLogs;
+    }
+
+    public override void ShowAlert(Action<bool> callback)
+    {
+        // Llama al plugn para mostrar la alerta
+        loggerObject.Call("ShowAlert");
+        callback.Invoke(true);
     }
 }
 
@@ -50,5 +104,21 @@ public class DefaultLogger : LoggerBase
     public override void Log(string str)
     {
         Debug.Log(str);
+    }
+
+    public override void DeleteAll()
+    {
+        Debug.Log("DeleteAll");
+    }
+
+    public override List<string> GetAllLogs()
+    {
+        Debug.Log("GetAllLogs");
+        return new List<string>();
+    }
+
+    public override void ShowAlert(Action<bool> callback)
+    {
+        throw new NotImplementedException();
     }
 }
