3d59f9c77091cc52adc7e13162b421df9d24c730
micalbenitez <benitezmicaelaluz@gmail.com>
2022-10-15 18:19:47 -0300

    Added condition to restart the game

--- MobileDevTP2_BenitezMicaela/Assets/Scripts/Managers/GameManager.cs
+++ MobileDevTP2_BenitezMicaela/Assets/Scripts/Managers/GameManager.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using TMPro;
 using Toolbox;
 
@@ -13,6 +14,7 @@ namespace Managers
 
         [Header("UI")]
         public TMP_Text scoreText = null;
+        public Image fadeImage = null;
 
         private int score = 0;
 
@@ -23,8 +25,30 @@ namespace Managers
 
         private void NewGame()
         {
+            Time.timeScale = 1f;
+            spawner.enabled = true;
+            blade.enabled = true;
+
             score = 0;
             scoreText.text = "Score: " + score;
+            ClearScene();
+        }
+
+        private void ClearScene()
+        {
+            Fruit[] fruits = FindObjectsOfType<Fruit>();
+
+            foreach (Fruit fruit in fruits)
+            {
+                Destroy(fruit.gameObject);
+            }
+
+            Bomb[] bombs = FindObjectsOfType<Bomb>();
+
+            foreach (Bomb bomb in bombs)
+            {
+                Destroy(bomb.gameObject);
+            }
         }
 
         public void UpdateScore(int points)
@@ -37,6 +61,40 @@ namespace Managers
         {
             spawner.enabled = false;
             blade.enabled = false;
+            StartCoroutine(ExplodeSequence());
+        }
+
+        private IEnumerator ExplodeSequence()
+        {
+            float elapsed = 0f;
+            float duration = 0.5f;
+
+            while (elapsed < duration)
+            {
+                float lerperTime = Mathf.Clamp01(elapsed / duration);
+                fadeImage.color = Color.Lerp(Color.clear, Color.black, lerperTime);
+
+                Time.timeScale = 1f - lerperTime;
+                elapsed += Time.unscaledDeltaTime;
+
+                yield return null;
+            }
+
+            yield return new WaitForSecondsRealtime(1f);
+
+            NewGame();
+
+            elapsed = 0;
+
+            while (elapsed < duration)
+            {
+                float lerperTime = Mathf.Clamp01(elapsed / duration);
+                fadeImage.color = Color.Lerp(Color.black, Color.clear, lerperTime);
+
+                elapsed += Time.unscaledDeltaTime;
+
+                yield return null;
+            }
         }
     }
 }
