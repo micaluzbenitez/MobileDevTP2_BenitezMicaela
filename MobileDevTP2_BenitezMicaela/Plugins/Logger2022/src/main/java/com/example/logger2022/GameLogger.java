11f080c38497d2ba8d79df0aaab2fdbf17f40783
micalbenitez <benitezmicaelaluz@gmail.com>
2022-10-19 12:11:04 -0300

    Updated android plugin

--- MobileDevTP2_BenitezMicaela/Plugins/Logger2022/src/main/java/com/example/logger2022/GameLogger.java
+++ MobileDevTP2_BenitezMicaela/Plugins/Logger2022/src/main/java/com/example/logger2022/GameLogger.java
@@ -4,7 +4,10 @@ import android.util.Log;
 
 public class GameLogger {
     private static final GameLogger instance = new GameLogger();
+
+    private static final String LOCAL_TAG = "l22 => ";
     private static final String TAG = "Log22 => ";
+
     public static GameLogger GetInstance() {
         Log.d("", "GetInstance()");
         return instance;
@@ -13,4 +16,22 @@ public class GameLogger {
     private GameLogger() { MyLog("Logger created"); }
 
     public void MyLog(String str) { Log.d(TAG, str); }
+
+    public void DeleteAll()
+    {
+        // Borra el archivo
+        Log.d(LOCAL_TAG, "DeleteAll");
+    }
+
+    public String GetAllLogs()
+    {
+        Log.d(LOCAL_TAG, "GetAllLogs");
+        // Lee el archivo y devuelve el contenido
+        return "";
+    }
+
+    public void ShowAlert()
+    {
+
+    }
 }
